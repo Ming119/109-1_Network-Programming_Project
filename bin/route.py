673302aacb0ad37161a7c9cbba6b5841d8a4417a
Ho-Ming Li <t108590050@ntut.edu.tw>
2021-01-09 14:58:41 +0800

    2021/01/09 14:58

--- bin/route.py
+++ bin/route.py
@@ -25,46 +25,63 @@ class Node:
 
 
 # Class: Graph
-# Weighted DAG
+# Weighted Graph
 class Graph:
     def __init__(self):
-        self.graph = collections.defaultdict(list);
+        self.edges = collections.defaultdict(list);
+        self.weights = {};
 
-    def insertEdge(self, node, neighbour, cost):
-        # {node:[(cost,neighbour), ...]}
-        self.graph[node].append((cost, neighbour));
-
-    # Find the Shortest Path
-    def shortestPath(self, _from, _dest):
-        # create a priority queue and hash set to store visited nodes
-        queue = [(0, _from, [])];    # Priority Queue
-        visited = set();
-        heapq.heapify(queue);
-
-        # BFS Traversal
-        while queue:
-            (cost, node, path) = heapq.heappop(queue);
-
-            # visit the node if the node is non-visited
-            if node not in visited:
-                visited.add(node);
-                path = path + [node];
-
-                # Hit the dest
-                if node == _dest: return (cost, path);
-
-                # visit neighbours
-                for c, neighbour in graph[node]:
-                    if neightbour not in visited:
-                        heapq.heappush(queue, (cost+c, neighbour, path));
-
-        # Path Not Found
-        return int("inf");
+    def insertEdge(self, fromNode, toNode, weight):
+        self.edges[fromNode].append(toNode);
+        self.edges[toNode].append(fromNode);
+        self.weights[(fromNode, toNode)] = weight;
+        self.weights[(toNode, fromNode)] = weight;
 
     # For Testing
     def toStr(self):
         for e in self.graph:
             print(e);
+
+
+# Function: dijsktra()
+# Find the Shortest Path by Dijsktra Algorithm
+def dijsktra(graph, initial, end):
+    # shortest paths is a dict of nodes
+    # whose value is a tuple of (previous node, weight)
+    shortest_paths = {initial: (None, 0)};
+    current_node = initial;
+    visited = set();
+
+    while current_node != end:
+        visited.add(current_node);
+        destinations = graph.edges[current_node];
+        weight_to_current_node = shortest_paths[current_node][1];
+
+        for next_node in destinations:
+            weight = graph.weights[(current_node, next_node)] + weight_to_current_node;
+            if next_node not in shortest_paths:
+                shortest_paths[next_node] = (current_node, weight);
+            else:
+                current_shortest_weight = shortest_paths[next_node][1];
+                if current_shortest_weight > weight:
+                    shortest_paths[next_node] = (current_node, weight);
+
+        next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited};
+        if not next_destinations:
+            return "Route Not Possible";
+        # next node is the destination with the lowest weight
+        current_node = min(next_destinations, key=lambda k: next_destinations[k][1]);
+
+    # Work back through destinations in shortest path
+    path = [];
+    while current_node is not None:
+        path.append(current_node);
+        next_node = shortest_paths[current_node][0];
+        current_node = next_node;
+    # Reverse path
+    path = path[::-1];
+    return path;
+
 
 
 # Function: arrangeData()
@@ -105,42 +122,73 @@ def arrangeData():
     return all_stations;
 
 
+
 # Function: constructRoute()
 def constructRoute():
-    stations = arrangeData();
+    route = arrangeData();
     graph = Graph();
 
-    travelTime = 0;
     with open("./API/MRT_Time_API.csv", 'r', encoding='BIG5') as f:
         timeData = list(csv.reader(f));
 
-        for key, val in stations.items():
-            for index, row in enumerate(timeData):
+        for field, stations in field.items():
+            for i in range(len(stations)-1):
+                travelTime = 0;
+                fromNode = statons[i];
+                toNode = stations[i+1];
 
-                # '台北車站' Needs special treatment
-                if (formNode.sName == '台北車站'):
-                    if (row[1][2:] == '台北車站') and (row[2][2:-1] == neighbour.sName) or \
-                        (row[2][2:] == '台北車站') and (row[1][2:-1] == neighbour.sName):
+                for index, row in enumerate(timeData):
 
-                        travelTime += (int(row[3]) + int(row[5]));
-                        if travelTime: timeData.pop(index);
-                        break;
-
-                elif (neighbour.sName == '台北車站'):
-                    if (row[1][2:] == '台北車站') and (row[2][2:-1] == formNode.sName) or \
-                        (row[2][2:] == '台北車站') and (row[1][2:-1] == formNode.sName):
+                    if (row[1][2:-1] == fromNode.sName) and (row[2][2:-1] == toNode.sName) or \
+                        (row[2][2:-1] == fromNode.sName) and (row[1][2:-1] == toNode.sName):
 
                         travelTime += (int(row[3]) + int(row[5]));
                         if travelTime: timeData.pop(index);
                         break;
 
-                elif (row[1][2:-1] == formNode.sName) and (row[2][2:-1] == neighbour.sName) or \
-                    (row[1][2:-1] == neighbour.sName) and (row[2][2:-1] == formNode.sName):
-                    travelTime += (int(row[3]) + int(row[5]));
+                if travelTime: graph.insertEdge(fromNode, toNode, travelTime);
 
-                    if travelTime: timeData.pop(index);
-                    break;
-            if travelTime: graph.insertEdge(formNode, neighbour, travelTime);
+                if fromNode.interchange: toNode = fromNode.interchange;
+                if fromNode == "台北車站": 
+
+                for index, row in enumerate(timeData):
+
+                    if (row[1][2:-1] == fromNode.sName) and (row[2][2:-1] == toNode.sName) or \
+                        (row[2][2:-1] == fromNode.sName) and (row[1][2:-1] == toNode.sName):
+
+                        travelTime += (int(row[3]) + int(row[5]));
+                        if travelTime: timeData.pop(index);
+                        break;
+
+
+
+
+
+
+                #     # '台北車站' Needs special treatment
+                #     if (station.sName == '台北車站'):
+                #         if (row[1][2:] == '台北車站') and (row[2][2:-1] == toNode.sName) or \
+                #             (row[2][2:] == '台北車站') and (row[1][2:-1] == toNode.sName):
+                #
+                #             travelTime += (int(row[3]) + int(row[5]));
+                #             if travelTime: timeData.pop(index);
+                #             break;
+                #
+                #     elif (toNode.sName == '台北車站'):
+                #         if (row[1][2:] == '台北車站') and (row[2][2:-1] == formNode.sName) or \
+                #             (row[2][2:] == '台北車站') and (row[1][2:-1] == formNode.sName):
+                #
+                #             travelTime += (int(row[3]) + int(row[5]));
+                #             if travelTime: timeData.pop(index);
+                #             break;
+                #
+                #     elif (row[1][2:-1] == formNode.sName) and (row[2][2:-1] == toNode.sName) or \
+                #         (row[1][2:-1] == toNode.sName) and (row[2][2:-1] == formNode.sName):
+                #         travelTime += (int(row[3]) + int(row[5]));
+                #
+                #         if travelTime: timeData.pop(index);
+                #         break;
+                # if travelTime: graph.insertEdge(formNode, toNode, travelTime);
 
     return graph;
 
@@ -165,9 +213,10 @@ def getStation(id = '', label = ''):
     print(" Error: getStation(), cannot find any matchabe station ")
     return None;
 
+
+
 if __name__ == '__main__':
     stations = arrangeData();
-
     graph = constructRoute();
 
 
