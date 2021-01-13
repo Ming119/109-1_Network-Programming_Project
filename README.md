a130c87dbcf9c426f14460b53e50d4303edde4e0
Ho-Ming Li <t108590050@ntut.edu.tw>
2021-01-13 21:02:54 +0800

    Update Web application

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ https://github.com/wntun/Dijkstra-shortest-path \
 Academic Journal of Engineering and Technology Science (ISSN 2616-5767) \
 By Francis Academic Press, UK \
 Subway Distribution Path Optimization Issues Based on Dijkstra Algorithm \
-https://francis-press.com/uploads/papers/pVs76FClOYAeJn0o7wjuXH7cAM6FgzOqDu8p8FDa.pdf 
+https://francis-press.com/uploads/papers/pVs76FClOYAeJn0o7wjuXH7cAM6FgzOqDu8p8FDa.pdf
 
 
 ## 功能說明
@@ -39,6 +39,6 @@ https://francis-press.com/uploads/papers/pVs76FClOYAeJn0o7wjuXH7cAM6FgzOqDu8p8FD
 輸出搭乘臺北捷運的最佳(所需時間最短)的捷運路線和所有票價供用戶參考。
 
 ## 專題優點、好處
-  1.可以把整條捷運路線印出來\
-  2.搭乘時間可準確到單位秒\
-  3.
+>1. 可以把整條捷運路線印出來\
+>2. 搭乘時間可準確到單位秒\
+>3. 跨平台
--- bin/app.py
+++ bin/app.py
@@ -12,24 +12,19 @@ app = Flask(__name__);
 stations = main.getStations();
 graph = main.constructRoute(stations);
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index(result = None):
-    return render_template('./index.html', stations = stations, result = result);
-
-@app.route('/result', methods = ['GET', 'POST'])
-def data():
     if request.method == 'POST':
       result = request.form;
-      print(result['startStationSelect']);
       start = main.findStationFromLabel(result['startStationSelect']);
       end = main.findStationFromLabel(result['endStationSelect']);
       path, time = main.dijsktra(graph, start, end);
       price = main.getPrice(start, end);
       mins = time//60;
       seconds = time%60;
+      result = (path, price, (mins, seconds))
 
-      return index(result = (path, price, (mins, seconds)));
-
+    return render_template('./index.html', stations = stations, result = result);
 
 
 
