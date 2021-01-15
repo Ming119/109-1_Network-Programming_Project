2c817333ecd490f99cc9f3ee68db6a4ae464862f
Ho-Ming Li <t108590050@ntut.edu.tw>
2021-01-15 14:41:25 +0800

    Update Web Application

--- bin/app.py
+++ bin/app.py
@@ -4,10 +4,12 @@
 # This is a Web Application for the main.py
 #
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+import secrets
 import main
 
 app = Flask(__name__);
+app.secret_key = secrets.token_urlsafe(32);
 
 stations = main.getStations();
 graph = main.constructRoute(stations);
@@ -15,18 +17,22 @@ graph = main.constructRoute(stations);
 @app.route('/', methods = ['GET', 'POST'])
 def index(result = None):
     if request.method == 'POST':
-      result = request.form;
-      start = main.findStationFromLabel(result['startStationSelect']);
-      end = main.findStationFromLabel(result['endStationSelect']);
-      path, time = main.dijsktra(graph, start, end);
-      price = main.getPrice(start, end);
-      mins = time//60;
-      seconds = time%60;
-      result = (path, price, (mins, seconds))
+        result = request.form;
+        if (result['startStationSelect'] == 'null' or result['endStationSelect'] == 'null'):
+            flash('請選擇車站');
+            return render_template('./index.html', stations = stations, result = 'error');
+
+        start = main.findStationFromLabel(result['startStationSelect']);
+        end = main.findStationFromLabel(result['endStationSelect']);
+        path, time = main.dijsktra(graph, start, end);
+        price = main.getPrice(start, end);
+        mins = time//60;
+        seconds = time%60;
+        result = (path, price, (mins, seconds))
 
     return render_template('./index.html', stations = stations, result = result);
 
 
 
 if __name__ == "__main__":
-    app.run(debug = False);
+    app.run(debug = True);
